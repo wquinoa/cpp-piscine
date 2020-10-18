cc6fe3bb77e5d54b10a61d77378ba3ce9c4e0f76
Shimat <philip.yevseev@gmail.com>
2020-10-18 05:05:19 +0300

    ex00 ex01 done

--- module_06/ex00/ex00.cpp
+++ module_06/ex00/ex00.cpp
@@ -1,27 +1,32 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   main.cpp                                           :+:      :+:    :+:   */
+/*   ex00.cpp                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: wquinoa <wquinoa@student.21-school.ru>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/10/17 22:01:50 by wquinoa           #+#    #+#             */
-/*   Updated: 2020/10/18 03:40:02 by wquinoa          ###   ########.fr       */
+/*   Updated: 2020/10/18 05:02:04 by wquinoa          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "main.hpp"
+#include "ex00.hpp"
 #define PRINT(x) (std::cout << x << std::endl)
+
+bool	g_badNumber = false;
+bool	g_isinf = false;
 
 bool	test_char(char* arg)
 {
 	char*	test;
     long c = strtol(arg, &test, 10);
 
-	if (!*test && c >= INT8_MIN && c <= INT8_MAX)
+	if (g_badNumber == true || g_isinf == true)
+		return (false);
+	if (c >= INT8_MIN && c <= INT8_MAX)
 	{
 		if (std::isprint(c))
-			std::cout << "'" << (char)(c) << "'" << std::endl;
+			std::cout << "'" << static_cast<char>(c) << "'" << std::endl;
 		else
 			std::cout << "Non displayable" << std::endl;
 		return (true);
@@ -32,13 +37,13 @@ bool	test_char(char* arg)
 bool	test_int(char* arg)
 {
 	char*	test;
-	long	value = strtol(arg, &test, 10);
+	double	value = strtod(arg, &test);
 
-	if (*test && *test != '.')
+	if (g_badNumber == true || g_isinf == true)
 		return (false);
 	if (value >= INT32_MAX || value <= (long)INT32_MIN)
 		return (false);
-	std::cout << (int)value << std::endl;
+	std::cout << static_cast<int>(value) << std::endl;
 	return (true);
 }
 
@@ -50,15 +55,20 @@ bool	test_float(char* arg)
 	if (*test)
 	{
 		if (*test != 'f' || *(test + 1))
+		{
+			g_badNumber = true;
 			return (false);
+		}
 	}
 	if (std::isnan(value))
 	{
 		std::cout << "nanf" << std::endl;
 		return (true);
 	}
-	std::cout << arg;
-	if (!strchr(arg, '.'))
+	if (std::isinf(value))
+		g_isinf = true;
+	std::cout << static_cast<float>(value);
+	if (value - static_cast<int>(value) == 0.0)
 		std::cout << ".0";
 	std::cout << "f" << std::endl;
 	return (true);
@@ -69,27 +79,25 @@ bool	test_double(char* arg)
 	char		*test = 0;
 	long double	value = strtold(arg, &test);
 
-	if (*test)
-	{
-		if (*test != 'f' || *(test + 1))
-			return (false);
-	}
+	if (g_badNumber == true)
+		return (false);
 	if (std::isnan(value))
 	{
+		g_badNumber = true;
 		std::cout << "nan" << std::endl;
 		return (true);
 	}
-	std::cout << arg;
-	if (!strchr(arg, '.'))
-		std::cout << ".0" << std::endl;
+	std::cout << static_cast<double>(value);
+	if (value - static_cast<int>(value) == 0.0)
+		std::cout << ".0";
 	std::cout << std::endl;
 	return (true);
 }
 
 void	route(char *arg)
 {
-    bool		(*tests[4])(char*) = { &test_char, &test_int, &test_float, &test_double };
-	std::string	tab[4] = { "char:   ", "int:    ", "float:  ", "double: " };
+    bool		(*tests[4])(char*) = { &test_float, &test_double, &test_char, &test_int };
+	std::string	tab[4] = { "float  : ", "double : ", "char   : ", "int    : "};
 
 	for (int i = 0; i < 4; i++)
 	{
@@ -99,7 +107,7 @@ void	route(char *arg)
 	}
 }
 
-int     main(int ac, char ** av)
+int     main(int ac, char **av)
 {
     if (ac != 2)
     {
---
+++ module_06/ex01/ex01.cpp
@@ -0,0 +1,58 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ex01.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: wquinoa <wquinoa@student.21-school.ru>     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2020/10/18 03:46:22 by wquinoa           #+#    #+#             */
+/*   Updated: 2020/10/18 04:11:11 by wquinoa          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "ex01.hpp"
+
+void	*serialize(void)
+{
+	Data			*data = new Data;
+	const char		alphanum[] =
+        			"0123456789"
+        			"ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+        			"abcdefghijklmnopqrstuvwxyz";
+
+	for (int i = 0 ; i < 8 ; i++)
+	{
+		data->s1 += alphanum[rand() % sizeof(alphanum)];
+		data->s2 += alphanum[rand() % sizeof(alphanum)];
+	}
+	data->n = rand();
+
+	std::cout << "\nThe serialized data :" << std::endl;
+	std::cout << "s1 : " << data->s1 << std::endl;
+	std::cout << "n  : " << data->n << std::endl;
+	std::cout << "s2 : " << data->s2 << std::endl;
+
+	return (static_cast<void *>(data));
+}
+
+Data	*deserialize(void *void_data)
+{
+	return (static_cast<Data *>(void_data));
+}
+
+int	main(void)
+{
+	void	*void_data;
+	Data	*data;
+
+	srand(time(0));
+
+	void_data = serialize();
+	data = deserialize(void_data);
+	std::cout << "\nThe deserialized data :" << std::endl;
+	std::cout << "s1 : " << data->s1 << std::endl;
+	std::cout << "n  : " << data->n << std::endl;
+	std::cout << "s2 : " << data->s2 << std::endl;
+	delete data;
+	return (0);
+}
