241623ed5b64305b1af6f80448d5af8d420d55e0
Shimat <wquinoa@student.21-school.ru>
2020-11-02 20:58:55 +0300

    addded test cases from the checklist -_-

--- module_07/ex00/whatever.cpp
+++ module_07/ex00/whatever.cpp
@@ -3,20 +3,40 @@
 /*                                                        :::      ::::::::   */
 /*   whatever.cpp                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: wquinoa <wquinoa@student.21-school.ru>     +#+  +:+       +#+        */
+/*   By: wquinoa <wquinoa@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/10/18 13:43:51 by wquinoa           #+#    #+#             */
-/*   Updated: 2020/10/18 14:11:54 by wquinoa          ###   ########.fr       */
+/*   Updated: 2020/11/02 20:50:03 by wquinoa          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <iostream>
 # define GREENIFY(x) ("\033[32m" + (std::string)x + "\033[0;m")
 
+class Awesome {
+public:
+Awesome( int n ) : _n( n ) {}
+	bool operator==( Awesome const & rhs ) { return (this->_n == rhs._n); }
+	bool operator!=( Awesome const & rhs ) { return (this->_n != rhs._n); }
+	bool operator>( Awesome const & rhs ) { return (this->_n > rhs._n); }
+	bool operator<( Awesome const & rhs ) { return (this->_n < rhs._n); }
+	bool operator>=( Awesome const & rhs ) { return (this->_n >= rhs._n); }
+	bool operator<=( Awesome const & rhs ) { return (this->_n <= rhs._n); }
+	int  get_n() const { return _n; }
+private:
+	int _n;
+};
+
+std::ostream &operator<< (std::ostream &stream, const Awesome &that)
+{
+	stream << that.get_n();
+	return (stream);
+}
+
 template <class T>
 void	swap(T &a, T &b)
 {
-	T	tmp;
+	T	tmp(0);
 
 	tmp = a;
 	a = b;
@@ -49,10 +69,19 @@ void    test(T a, T b)
 
 int		main(void)
 {
+	Awesome a(123);
+	Awesome b(321);
+
+	swap(a, b);
+	min(a, b);
+	max(a, b);
+	test(a, b);
     test(3, 2);
     test("chaine1", "chaine2");
     test(4.2, 2.1);
     test('A', 'Z');
+    test(0, 0);
+    test(4000000000000, 4000000000001);
 
 	return (0);
 }
--- module_07/ex01/iter.cpp
+++ module_07/ex01/iter.cpp
@@ -6,30 +6,50 @@
 /*   By: wquinoa <wquinoa@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/10/18 14:13:15 by wquinoa           #+#    #+#             */
-/*   Updated: 2020/10/31 06:31:57 by wquinoa          ###   ########.fr       */
+/*   Updated: 2020/11/02 20:56:58 by wquinoa          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <iostream>
 
+class Awesome
+{
+public:
+    Awesome( void ) : _n( 42 ) { return; }
+    int get( void ) const { return this->_n; }
+private:
+    int _n;
+};
+
+std::ostream & operator<<( std::ostream & o, Awesome const & rhs )
+{
+    o << rhs.get();
+    return o; 
+}
+
+template< typename T >
+void print( T const & x ) 
+{
+    std::cout << x << std::endl; return; 
+}
+
 template <class T> 
-T       function(T &item)
+void       function(T &item)
 {
-    return (item - 1);
+    std::cout << static_cast<T>(item - 1) << " ";
 }
 
 template <class T>
-T       arrays(T &item)
+void       arrays(T &item)
 {
-    return (item);
+    std::cout << item << " ";
 }
 
 template <class T>
-void    iter(T *tab, int len, T f(T &))
+void    iter(T *tab, int len, void f(T const &))
 {
     for (int i = 0; i < len; i++)
-        std::cout << f(tab[i]) << " ";
-    std::cout << std::endl; 
+       f(tab[i]);
 }
 
 int main(void)
@@ -38,11 +58,21 @@ int main(void)
     int             goodbye[] = {1, 2, 3, 4, 5};
     float           hey[] = {1.2, 2.3, 3.4, 4.5, 5.6};
     std::string     please[] = { "Give", "me a", "100", "right", "now" };
+    int tab[] = { 0, 1, 2, 3, 4 }; // <--- I never understood why you can't write int[] tab. Wouldn't that make more sense?
+    Awesome tab2[5];
+
+    iter( tab, 5, print );
+    iter( tab2, 5, print );
+    std::cout << std::endl;
 
     iter(hello, 5, function);
+    std::cout << std::endl;
     iter(goodbye, 5, function);
+    std::cout << std::endl;
     iter(hey, 5, function);
+    std::cout << std::endl;
     iter(please, 5, arrays);
+    std::cout << std::endl;
 
     return(0);
 }
