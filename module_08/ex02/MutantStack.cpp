05c5a093bd2fe1926ca54894e4b8696eaad5e070
Shimat <wquinoa@student.21-school.ru>
2020-11-09 20:12:35 +0300

    slight clean up

--- module_08/ex02/MutantStack.cpp
+++ module_08/ex02/MutantStack.cpp
@@ -6,26 +6,10 @@
 /*   By: wquinoa <wquinoa@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/11/09 16:37:08 by wquinoa           #+#    #+#             */
-/*   Updated: 2020/11/09 19:47:47 by wquinoa          ###   ########.fr       */
+/*   Updated: 2020/11/09 20:09:39 by wquinoa          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "MutantStack.hpp"
 
-
-//MutantStack::MutantStack()
-//{
-//}
-
-//MutantStack::~MutantStack()
-//{
-//}
-
-//MutantStack::MutantStack(const MutantStack &copy)
-//{
-//}
-
-//MutantStack	&MutantStack::operator=(const MutantStack &copy)
-//{
-	//return (*this);
-//}
+// i dont need no cpp file
